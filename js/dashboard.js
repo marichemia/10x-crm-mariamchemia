cb234c90ee26fe24e3919c0d87219e1605614e99
marichemia <marichemia10@gmail.com>
2026-07-16 18:40:36 +0400

    feat: add current time and date display to dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,6 +3,7 @@ import {clearSession, getUsers} from './storage.js';
 
 const session = redirectIfNotAuthenticated();
 
+//display current user name
 if (session) {
     const users = getUsers();
     const user = users.find(u => u.id === session.userId);
@@ -14,7 +15,22 @@ if (session) {
     }
 }
 
+//display time and date
+const dateElement = document.getElementById('current-date');
+const timeElement = document.getElementById('current-time');
 
+function displayDateTime() {
+    const currentDate = new Date();
+
+    dateElement.textContent = currentDate.toLocaleDateString();
+    timeElement.textContent = currentDate.toLocaleTimeString();
+}
+
+displayDateTime();
+setInterval(displayDateTime, 1000); //update every sec
+
+
+//logout and clear session
 document.getElementById('logout-button').addEventListener('click', () => {
     clearSession();
     window.location.href = './index.html';
