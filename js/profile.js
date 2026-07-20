573ff116e79559efc25b986684e243c835aba42b
marichemia <marichemia10@gmail.com>
2026-07-20 21:11:04 +0400

    style: added _profile.scss partial with profile page styles

--- js/profile.js
+++ js/profile.js
@@ -1,2 +1,17 @@
 import {session} from "./shared.js";
+import { getUsers } from "./storage.js";
 console.log(session) //testing
+
+const nameInputElement = document.getElementById("profile-full-name");
+const emailInputElement = document.getElementById("profile-email");
+let users = getUsers();
+
+//get current user from active session
+let currentUser = users.find(user => user.id === session.userId);
+
+if(currentUser) {
+    nameInputElement.value = currentUser.fullName;
+    emailInputElement.value = currentUser.email;
+}
+
+
